7c4ceab7cfd376fc1c697ac28a8a51eab1ed18f1
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-02 21:49:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,13 @@
 class Application
   
   def call(env)
+    resp = Rack::Response.new
     
+    if time.now < 12 
+      resp.write "Good Morning!"
+    else
+      resp.wrote "Good Afternoon!"
+    end 
   end
   
   
